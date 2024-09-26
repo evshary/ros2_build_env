90cdd8bb3a151664fd2f1968a9e1f121be10fe86
ChenYing Kuo <evshary@gmail.com>
2024-09-26 18:01:17 +0800

    Able to run rmw_zenoh inside the containers
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- containers/ros2-jazzy-gui/run_container.sh
+++ containers/ros2-jazzy-gui/run_container.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 
-DOCKER_IMAGE=jazzy-gui
+DOCKER_IMAGE=ros2-jazzy-gui
 DOCKER_FILE=containers/ros2-jazzy-gui/Dockerfile
 
 if [ ! "$(docker images -q ${DOCKER_IMAGE})" ]; then
     echo "${DOCKER_IMAGE} does not exist. Creating..."
-    docker build -f ${DOCKER_FILE} -t ${DOCKER_IMAGE} .
+    docker build --build-arg UID=`id -u` \
+                 --build-arg GID=`id -g` \
+                 --build-arg USERNAME=${USER} \
+                 -f ${DOCKER_FILE} -t ${DOCKER_IMAGE} .
 fi
 
-rocker --network host --privileged --x11 --user --volume $(pwd):$HOME/workspace -- ${DOCKER_IMAGE}
+#rocker --network host --privileged --x11 --user --volume $(pwd):$HOME/workspace -- ${DOCKER_IMAGE}
+xhost +local:
+docker run --rm -it --network host  --privileged -v $(pwd):$HOME/workspace --workdir $HOME/workspace \
+    -e QT_X11_NO_MITSHM=1 -e DISPLAY=$DISPLAY \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v /etc/localtime:/etc/localtime:ro \
+    ${DOCKER_IMAGE}
--- scripts/ros2-jazzy-rmw-zenoh/README.md
+++ scripts/ros2-jazzy-rmw-zenoh/README.md
@@ -2,23 +2,29 @@
 
 Able to run GUI ROS 2 program in containers.
 
-## Prerequisites
-
-* Install rocker first.
-
 ## Step
 
 * Run docker container
 
 ```shell
-./jazzy_gui/run_container.sh
+./containers/ros2-jazzy-gui/run_container.sh
 ```
 
-* Run navigation2
+* Build the code
 
 ```shell
+just scripts/ros2-jazzy-rmw-zenoh/all
+```
+
+* Test navigation2
+
+```shell
+# terminal 1
+ros2 run rmw_zenoh_cpp rmw_zenohd
+# terminal 2
 export TURTLEBOT3_MODEL=waffle
 export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/jazzy/share/turtlebot3_gazebo/models
 export LIBGL_ALWAYS_SOFTWARE=1 # Necessary if GPU has some issues
+export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 ros2 launch nav2_bringup tb3_simulation_launch.py headless:=False
 ```
