173c2db8f2e5db254dfc54ca8f3520adf42ec4c0
ChenYing Kuo <evshary@gmail.com>
2024-10-04 14:18:40 +0800

    Rebuild the container with no-cache.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- run_container.sh
+++ run_container.sh
@@ -5,14 +5,15 @@ echo "test ${DOCKER_IMAGE}"
 
 if [ ! "$(docker images -q ${DOCKER_IMAGE})" ]; then
     echo "${DOCKER_IMAGE} does not exist. Creating..."
-    docker build --build-arg UID=`id -u` \
+    docker build --no-cache \
+                 --build-arg UID=`id -u` \
                  --build-arg GID=`id -g` \
                  --build-arg USERNAME=${USER} \
                  -f ${DOCKER_FILE} -t ${DOCKER_IMAGE} .
 fi
 
 xhost +local:
-docker run --rm -it --network host  --privileged -v $(pwd):$HOME/workspace --workdir $HOME/workspace \
+docker run --rm -it --network host --privileged -v $(pwd):$HOME/workspace --workdir $HOME/workspace \
     -e QT_X11_NO_MITSHM=1 -e DISPLAY=$DISPLAY \
     -v /tmp/.X11-unix:/tmp/.X11-unix \
     -v /etc/localtime:/etc/localtime:ro \
