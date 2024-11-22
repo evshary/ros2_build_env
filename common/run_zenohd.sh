3476c4b810508423130225ce31216eef5ef680ea
ChenYing Kuo <evshary@gmail.com>
2024-11-22 15:54:25 +0800

    Add an easier way to run zenohd
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

---
+++ common/run_zenohd.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -x
+
+source /opt/ros/"${ROS_DISTRO}"/setup.bash
+source ros2_"${ROS_DISTRO}"_rmw_zenoh_ws/install/setup.bash
+export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+RUST_LOG=z=debug ros2 run rmw_zenoh_cpp rmw_zenohd
