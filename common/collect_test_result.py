f53128038cc47d93ae9df0e7659a440c00c453df
ChenYing Kuo <evshary@gmail.com>
2024-12-24 14:20:14 +0800

    Update the way to collect result.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- common/collect_test_result.py
+++ common/collect_test_result.py
@@ -1,6 +1,17 @@
 import os
 import re
 import sys
+from collections import defaultdict
+
+exception_list = [
+    # test_quality_of_service
+    "test_deadline__rmw_zenoh_cpp",
+    "test_liveliness__rmw_zenoh_cpp",
+    "test_best_available__rmw_zenoh_cpp",
+    # test_rclcpp (Everything related to FastDDS and CycloneDDS)
+    "fastrtps",
+    "cyclonedds",
+]
 
 
 def strip_ansi_escape_sequences(text):
@@ -59,19 +70,78 @@ def collect_test_results(base_dir, log_filename="stdout_stderr.log"):
     return result_data
 
 
-if len(sys.argv) < 2:
-    print("Please assign the folde you want to collect logs")
-    sys.exit()
+def export_full_result(filename, collected_results):
+    with open(filename, "w") as output_file:
+        for file_path, content in collected_results.items():
+            output_file.write(f"Results from: {file_path}\n\n")
+            output_file.write(content)
+            output_file.write("\n" + "=" * 80 + "\n")
 
-base_directory = sys.argv[1] + "/log/latest_test/"
-collected_results = collect_test_results(base_directory)
 
-# Save results to a summary file or print them
-summary_file = "log_" + sys.argv[1] + ".log"
-with open(summary_file, "w") as output_file:
+def covert_to_dict(collected_results):
+    # Initialize a defaultdict to store results
+    results = defaultdict(list)
+
+    # Regular expression patterns for extracting information
+    failed_tests_pattern = re.compile(
+        r"The following tests FAILED:\n((?:\t\s*\d+ - .+? \(.*?\)\n?)+)"
+    )
+    individual_failed_test_pattern = re.compile(r"\t\s*(\d+ - .+? \(.*?\))")
+
     for file_path, content in collected_results.items():
-        output_file.write(f"Results from: {file_path}\n\n")
-        output_file.write(content)
-        output_file.write("\n" + "=" * 80 + "\n")
+        package_name = file_path.split("/")[-2]
+        failed_tests_block = failed_tests_pattern.search(content)
+        if failed_tests_block:
+            individual_failed_tests = individual_failed_test_pattern.findall(
+                failed_tests_block.group(1)
+            )
+            if individual_failed_tests:
+                results[package_name].extend(individual_failed_tests)
 
-print(f"Collected results saved to {summary_file}")
+    # Convert defaultdict to a regular dictionary
+    return dict(results)
+
+
+def export_filtered_result(filename, filtered_result):
+    output = ""
+    fail_test_num = 0
+    for pkg, fail_list in filtered_result.items():
+        for f in fail_list:
+            skip = False
+            for e in exception_list:
+                if e in f:
+                    skip = True
+                    break
+            if not skip:
+                output += pkg + ": " + f + "\n"
+                fail_test_num += 1
+    if fail_test_num != 0:
+        output += "Failure number: " + str(fail_test_num)
+    with open(filename, "w") as output_file:
+        output_file.write(output)
+    return (fail_test_num, output)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Please assign the folde you want to collect logs")
+        sys.exit()
+
+    # Collect result from all directories
+    base_directory = sys.argv[1] + "/log/latest_test/"
+    collected_results = collect_test_results(base_directory)
+
+    # Save results to a summary file
+    export_filename = "log_" + sys.argv[1] + "_full.log"
+    export_full_result(export_filename, collected_results)
+
+    # Covert results to dict
+    results_dict = covert_to_dict(collected_results)
+
+    # Print out the results can filter the exception one
+    export_filename = "log_" + sys.argv[1] + "_simplified.log"
+    (failure_num, result) = export_filtered_result(export_filename, results_dict)
+
+    # Check results
+    if failure_num != 0:
+        print(result)
