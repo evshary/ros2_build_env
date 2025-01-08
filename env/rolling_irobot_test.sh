444e7256a3610a0a75b1c63b6d7abddb9f3a18d2
ChenYing Kuo <evshary@gmail.com>
2025-01-08 17:56:08 +0800

    Add new env.sh
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

---
+++ env/rolling_irobot_test.sh
@@ -0,0 +1,14 @@
+shell=`echo $SHELL | awk -F '/' '{print $NF}'`
+rosdistro=rolling
+
+source /opt/ros/${rosdistro}/setup.${shell}
+source ros2_${rosdistro}_rmw_zenoh_ws/install/local_setup.${shell}
+source ros2_${rosdistro}_irobot_test_ws/install/local_setup.${shell}
+
+export RMW_IMPLEMENTATION=rmw_zenoh_cpp
+
+# Configuration for Zenoh
+#export ZENOH_SESSION_CONFIG_URI=xxxx
+#export ZENOH_ROUTER_CONFIG_URI=xxxx
+#export ZENOH_SHM_ENABLED=true
+#export ZENOH_SHM_MESSAGE_SIZE_THRESHOLD=1
