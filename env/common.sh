9376a6730ecf1ce89efe8243c708e31bc1e6e4e9
ChenYing Kuo <evshary@gmail.com>
2025-01-10 11:33:52 +0800

    Support ccache.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- env/common.sh
+++ env/common.sh
@@ -11,3 +11,10 @@ export RMW_IMPLEMENTATION=rmw_zenoh_cpp
 #export ZENOH_ROUTER_CONFIG_URI=xxxx
 #export ZENOH_SHM_ENABLED=true
 #export ZENOH_SHM_MESSAGE_SIZE_THRESHOLD=1
+
+# ccache configuration
+# Refer to https://autowarefoundation.github.io/autoware-documentation/main/how-to-guides/others/advanced-usage-of-colcon/#using-ccache-to-speed-up-recompilation
+export CC="/usr/lib/ccache/gcc"
+export CXX="/usr/lib/ccache/g++"
+export CCACHE_DIR="$HOME/.cache/ccache/"
+ccache -s # Show the current cache
