9eafb37982e39bfe65cfa6eb55a05a458bad50e2
ChenYing Kuo <evshary@gmail.com>
2025-01-13 10:32:41 +0800

    Support different terminal names.
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- env/env.sh
+++ env/env.sh
@@ -13,3 +13,5 @@ for folder in ros2_${rosdistro}_*_ws; do
     echo "Sourcing $folder/install/local_setup.${shell}"
     source $folder/install/local_setup.${shell}
 done
+
+PS1=$PS1"(${rosdistro}) "
--- env/rolling_irobot_test.sh
+++ env/rolling_irobot_test.sh
@@ -6,3 +6,5 @@ rosdistro=rolling
 source /opt/ros/${rosdistro}/setup.${shell}
 source ros2_${rosdistro}_rmw_zenoh_ws/install/local_setup.${shell}
 source ros2_${rosdistro}_irobot_performance_ws/install/local_setup.${shell}
+
+PS1=$PS1"($(basename "$0" | cut -d '.' -f 1)) "
