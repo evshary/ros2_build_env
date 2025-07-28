5987a4380dbf78c8d1d73bb75ebefe68caabf2d9
ChenYing Kuo <evshary@gmail.com>
2025-07-28 17:08:33 +0800

    Add comments for ROS 2 humble
    Signed-off-by: ChenYing Kuo <evshary@gmail.com>

--- common/run_nav2_tb3.sh
+++ common/run_nav2_tb3.sh
@@ -4,6 +4,10 @@ set -x
 source /opt/ros/"${ROS_DISTRO}"/setup.bash
 source ros2_"${ROS_DISTRO}"_nav2_ws/install/local_setup.bash
 
+## Only required the version before ROS 2 Iron
+#export TURTLEBOT3_MODEL=waffle
+#export GAZEBO_MODEL_PATH=$GAZEBO_MODEL_PATH:/opt/ros/${ROS_DISTRO}/share/turtlebot3_gazebo/models
+
 ros2 launch nav2_bringup tb3_simulation_launch.py headless:=False use_rviz:=False 2>&1 | tee nav2_tb3.log
 
 ## You might need this if using WAYLAND
